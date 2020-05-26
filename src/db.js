97efce23871c383a7f83a77057c78fd77595b549
Tam Le <tamtale@gmail.com>
2020-05-26 17:53:43 -0500

    fix mongoose deprecation warnings

--- src/db.js
+++ src/db.js
@@ -3,7 +3,14 @@ let { MONGODB_CONNECTION_STRING } = require("./config");
 
 const url = MONGODB_CONNECTION_STRING;
 
-mongoose.connect(url);
+// Avoid deprecation warnings: https://mongoosejs.com/docs/deprecations.html
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}
+
+mongoose.connect(url, options);
 
 mongoose.connection.on("connected", function() {
   console.log("Mongoose connected to " + url);
